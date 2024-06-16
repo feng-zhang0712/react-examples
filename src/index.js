b6d297c798d17e3c09254d96935f3006a650abf5
mkjfeng01 <592676493@qq.com>
2024-06-16 17:46:13 +0800

    config project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+  return <div>React Examples</div>;
+};
+
+ReactDOM.createRoot(document.getElementById('root'))
+.render(
+  <App />
+);
