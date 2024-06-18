9208b93363b89996942edead7671b88e30e5cf6a
feng.zhang <zfeng0712@gmail.com>
2024-06-19 00:18:29 +0800

    update state

---
+++ src/examples/hooks/state/Counter.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <button onClick={handleClick}>
+      You press me {count} times
+    </button>
+  );
+};
+
+export default Counter;
--- src/examples/hooks/state/index.js
+++ src/examples/hooks/state/index.js
@@ -1,15 +1,1 @@
-import React, { useState } from 'react';
-
-const counter = () => {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      {count}
-      <button onClick={setCount(count++)}>+</button>
-      <button onClick={setCount(count--)}>-</button>
-    </div>
-  );
-};
-
-export default counter;
+export { default as Counter } from './Counter';
